65dae8b306495f2cb0b65f202a693bda25e15a4c
yujinghui <jinghuiace@gmail.com>
2021-07-24 19:24:09 +0800

    编程之美3.4

---
+++ src/bop/3.4.cc
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void deleteRandomNode(ListNode *ptr)
+{
+    if (ptr == nullptr)
+    {
+        cout << "prt is null" << endl;
+    }
+    ListNode *ptrNext = ptr->next;
+    ptr->next = ptrNext->next;
+    ptr->val = ptrNext->val;
+    delete ptrNext;
+}
+int main()
+{
+    ListNode *l1 = new ListNode(21);
+    ListNode *l2 = new ListNode(17, l1);
+    ListNode *l3 = new ListNode(16, l2);
+    ListNode *l4 = new ListNode(15, l3);
+    ListNode *l5 = new ListNode(9, l4);
+    ListNode *p = l5;
+    while (p != nullptr)
+    {
+        cout << p->val << endl;
+        p = p->next;
+    }
+    p = l5;
+    deleteRandomNode(l3);
+    while (p != nullptr)
+    {
+        cout << p->val << endl;
+        p = p->next;
+    }
+}
